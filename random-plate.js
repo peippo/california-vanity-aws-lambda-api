fda94aee11c58ed38d0770c9f01c5cf049bf36c8
Janne Peippo <peippo@lumous.com>
2020-03-22 21:07:56 +0200

    Separate functions to own files

--- random-plate.js
+++ random-plate.js
@@ -45,22 +45,3 @@ export async function random(event, context) {
 		return failure({ status: false });
 	}
 }
-
-export async function single(event, context) {
-	const params = {
-		TableName: process.env.TABLE_NAME,
-		Key: { plate: event.pathParameters.plate }
-	};
-
-	try {
-		const result = await dynamoDBService.documentClient("get", params);
-
-		if (result.Item) {
-			return success(result.Item);
-		} else {
-			return failure({ status: false, error: "Plate not found" });
-		}
-	} catch (e) {
-		return failure({ status: false });
-	}
-}
---
+++ single-plate.js
@@ -0,0 +1,21 @@
+import * as dynamoDBService from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function single(event, context) {
+	const params = {
+		TableName: process.env.TABLE_NAME,
+		Key: { plate: event.pathParameters.plate }
+	};
+
+	try {
+		const result = await dynamoDBService.documentClient("get", params);
+
+		if (result.Item) {
+			return success(result.Item);
+		} else {
+			return failure({ status: false, error: "Plate not found" });
+		}
+	} catch (e) {
+		return failure({ status: false });
+	}
+}
